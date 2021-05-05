1245c47a0240d4cb3f30b2555aaf99421594620b
Rebecca Swernofsky <swernofsky.r@northeastern.edu>
2021-05-04 22:34:38 -0400

    add more dependencies

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ const StyledButton = styled.button`
   width: 10rem;
   border-radius: 0.5rem;
   border: solid 0 transparent;
-
-  :active {
-    border: solid 0 transparent;
-  }
 `;
 
-export default function App() {
+const App = () => {
+  const test = () => {
+    console.log('Hello');
+  }
   return (
     <div className="App">
-      <StyledButton>Hello!</StyledButton>
+      <StyledButton
+        onClick={test}
+      >
+        Hello!
+      </StyledButton>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
-};
+}
+
+export default App;
